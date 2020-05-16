40a1fefb075db2fdf2cab4aef6e07824576b9e9c
Jordan Le <jordan.le.official@gmail.com>
2020-05-16 11:18:08 -0700

    Removed unused, comment updates, little clean up.

--- manual_nn.py
+++ manual_nn.py
@@ -8,68 +8,6 @@ from configs import *
 from os import listdir
 from os.path import isfile, join
 
-def get_network_arch(individual):
-    '''
-    Function that takes in the weights of a specific individual and converts them to the correct model architecture.
-    :param individual: The weights of a specific individual, 1d numpy array
-    :return: Corrected Model architecture
-    '''
-    # Weight Matrix 1, the flattened first section of the weights list
-    w_matrix_1 = individual[0:W1_SHAPE[0] * W1_SHAPE[1]]
-    # Weight Matrix 2, the flattened second section of the weights list
-    w_matrix_2 = individual[W1_SHAPE[0] * W1_SHAPE[1]:W2_SHAPE[0] * W2_SHAPE[1] + W1_SHAPE[0] * W1_SHAPE[1]]
-    # Weight Matrix 3, the flattened third section of the weights list
-    w_matrix_3 = individual[W2_SHAPE[0] * W2_SHAPE[1] + W1_SHAPE[0] * W1_SHAPE[1]:]
-
-    return w_matrix_1.reshape(W1_SHAPE[0], W1_SHAPE[1]),\
-           w_matrix_2.reshape(W2_SHAPE[0], W2_SHAPE[1]),\
-           w_matrix_3.reshape(W3_SHAPE[0], W3_SHAPE[1])
-
-
-def softmax(z):
-    # Softmax activation layer
-    s = np.exp(z.T) / np.sum(np.exp(z.T), axis=1).reshape(-1, 1)
-    return s
-
-
-def sigmoid(z):
-    s = 1 / (1 + np.exp(-z))
-    return s
-
-
-def relu(z):
-    return np.maximum(0, z)
-
-
-def sigmoid_backward(dA, Z):
-    sig = sigmoid(Z)
-    return dA * sig * (1 - sig)
-
-
-def relu_backward(dA, Z):
-    dZ = np.array(dA, copy = True)
-    dZ[Z <= 0] = 0
-    return dZ
-
-# This is where I'll make predictions
-def forward_propagation(State, ind_weight):
-    '''
-    Function used to make predictions given a state and the specific weight values.
-    :param State: Numpy array of 0's and 1's
-    :param ind_weight: Numpy array of weight values between -1 and 1
-    :return:
-    '''
-    w_matrix_1, w_matrix_2, w_matrix_3 = get_network_arch(ind_weight)
-
-    Z1 = np.matmul(w_matrix_1, State.T)
-    A1 = np.tanh(Z1)                        # Hyperbolic tangent activation given Z1
-    Z2 = np.matmul(w_matrix_2, A1)
-    A2 = np.tanh(Z2)                        # Hyperbolic tangent activation given Z2
-    Z3 = np.matmul(w_matrix_3, A2)
-    A3 = softmax(Z3)
-    return A3
-
-
 def save_weights_as_csv(weights, file_name, file_path="weight_files/numpy_weight_files"):
     '''
     Save the weights of any given model from the dino brains to a csv file.
@@ -106,3 +44,60 @@ def load_all_networks(file_path='weight_files/numpy_weight_files'):
 
     for id, weight in enumerate(all_weights):
         DINO_BRAINS[id] = load_saved_weight_csv(weight)                # Use all the previous saved dinos
+
+
+def get_network_arch(individual):
+    '''
+    Function that takes in the weights of a specific individual and converts them to the correct model architecture.
+    :param individual: The weights of a specific individual, 1d numpy array
+    :return: Corrected Model architecture
+    '''
+    # Get the initial cut off points for the flattened matrices
+    m1_dims = W1_SHAPE[0] * W1_SHAPE[1]
+    m2_dims = W2_SHAPE[0] * W2_SHAPE[1]
+    # Weight Matrix 1, the flattened first section of the weights list
+    w_matrix_1 = individual[0:m1_dims]
+    # Weight Matrix 2, the flattened second section of the weights list
+    w_matrix_2 = individual[m1_dims:m2_dims + m1_dims]
+    # Weight Matrix 3, the flattened third section of the weights list
+    w_matrix_3 = individual[m2_dims + m1_dims:]
+
+    return w_matrix_1.reshape(W1_SHAPE[0], W1_SHAPE[1]),\
+           w_matrix_2.reshape(W2_SHAPE[0], W2_SHAPE[1]),\
+           w_matrix_3.reshape(W3_SHAPE[0], W3_SHAPE[1])
+
+
+def softmax(z):
+    '''
+    Softmax activation layer, takes in the output of the matrix multiply, returns percentages for each output index
+    :param z: Unbounded matrix of output values from matrix multiply
+    :return: percentages for each output index
+    '''
+    s = np.exp(z.T) / np.sum(np.exp(z.T), axis=1).reshape(-1, 1)
+    return s
+
+
+# This is where I'll make predictions
+def forward_propagation(State, ind_weight):
+    '''
+    Function used to make predictions given a state and the specific weight values.
+    :param State: Numpy array of 0's and 1's
+    :param ind_weight: Numpy array of weight values between -1 and 1
+    :return: numpy array of size output array, max value is the move
+    '''
+    w_matrix_1, w_matrix_2, w_matrix_3 = get_network_arch(ind_weight)
+
+    # Inital input layer takes in the first matrix with the given state, provides output to squash
+    Z1 = np.matmul(w_matrix_1, State.T)     # Creates unbounded output from given state and matrix multiply
+    A1 = np.tanh(Z1)                        # Hyperbolic tangent activation given Z1
+
+    # Second layer takes the squashed input from the first layer (values between -1, 1), then provides output to squash
+    Z2 = np.matmul(w_matrix_2, A1)          # Creates unbounded output from given tanh output and matrix multiplies
+    A2 = np.tanh(Z2)                        # Hyperbolic tangent activation given Z2
+
+    # Third and final layer takes the (-1, 1 inputs from second layer) and outputs it into percentage values of each
+    Z3 = np.matmul(w_matrix_3, A2)          # Creates unbounded output from tanh of second layer to matrix multiply
+    A3 = softmax(Z3)                        # Provides percentages for each of the potential outputs
+
+    return A3   # Returns the softmax activation. Ie what move we'll make
+
